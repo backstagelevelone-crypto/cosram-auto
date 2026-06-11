8b4c1ba1cfcb52a67bd1d47aa0f104974c38386d
backstagelevelone-crypto <backstagelevelone@gmail.com>
2026-06-11 08:27:32 +0300

    Update masini.ts

--- cosram-auto-main/src/data/masini.ts
+++ cosram-auto-main/src/data/masini.ts
@@ -81,7 +81,7 @@ export const masini: Masina[] = [
     "motor": "1,8",
     "putere": 140,
     "caroserie": "Combi",
-    "disponibil": "VANDUT",
+    "disponibil": "Vandut",
     "galerie": [
       "/images/masini/zafira7locuri20091.8benzina.png"
     ]

49ca4d8a68f88b396af159094a575edc95a5e6a5
backstagelevelone-crypto <backstagelevelone@gmail.com>
2026-06-16 13:28:26 +0300

    Update MasiniLaComanda.tsx

--- cosram-auto-main/src/components/sections/MasiniLaComanda.tsx
+++ cosram-auto-main/src/components/sections/MasiniLaComanda.tsx
@@ -70,7 +70,7 @@ export default function MasiniLaComanda() {
             </h3>
 
             <p className="text-[#6B6B6B]">
-              Ne ocupăm de transport și documentele necesare până la predare.
+              Ne ocupăm de transport și documentele necesare până la predare.Transportam masina cu platformele noastre proprii.Nu lucram cu transportatori externi.
             </p>
           </div>
         </div>

4d57e0c19a56f2dac957e2c89a8c5ce802983e23
backstagelevelone-crypto <backstagelevelone@gmail.com>
2026-07-11 18:36:40 +0300

    Update masini.ts

--- cosram-auto-main/src/data/masini.ts
+++ cosram-auto-main/src/data/masini.ts
@@ -70,7 +70,7 @@ export const masini: Masina[] = [
       "/images/masini/bmw-320d-mpacket-2006/02.jpg",
       "/images/masini/bmw-320d-mpacket-2006/03.jpg",
       "/images/masini/bmw-320d-mpacket-2006/04.jpg",
-      "/images/masini/bmw-320d-mpacket-2006/06.jpg",
+      "/images/masini/bmw-320d-mpacket-2006/06.jpg"
     ]
   },
   {
@@ -87,15 +87,15 @@ export const masini: Masina[] = [
     "caroserie": "Combi",
     "disponibil": "Disponibil",
     "galerie": [
-      "/images/masini/audia4b7/a41.jpeg"
-      "/images/masini/audia4b7/a42.jpeg"
-      "/images/masini/audia4b7/a43.jpeg"
-      "/images/masini/audia4b7/a44.jpeg"
+      "/images/masini/audia4b7/a41.jpeg",
+      "/images/masini/audia4b7/a42.jpeg",
+      "/images/masini/audia4b7/a43.jpeg",
+      "/images/masini/audia4b7/a44.jpeg",
       "/images/masini/audia4b7/a46.jpeg"
     ]
   },
   {
-    "_id": "fe57e922-c8d8-4657-8f1f-4b4ab98861c6",
+    "_id": "fe57e922-c8d8-4657-8f1f-4b4ab98861d7",
     "slug": "bmw-320d-mpacket-2006",
     "marca": "BMW 320D",
     "model": "MPACKET",
@@ -111,7 +111,7 @@ export const masini: Masina[] = [
     ]
   },
   {
-    "_id": "aeb998d7-52e9-411b-8472-44cc16d43fb3",
+    "_id": "aeb998d7-52e9-411b-8472-44cc16d43fc4",
     "slug": "vw-passat-highline-2009",
     "marca": "Opel",
     "model": "Zafira",
@@ -237,7 +237,7 @@ export const masini: Masina[] = [
   },
   {
     "_id": "f4c248f4-8aae-47ea-9492-b984c05bfcc9",
-    "slug": "skoda-fabia-2009 (x2)",
+    "slug": "skoda-fabia-2009-x2",
     "marca": "SKODA ",
     "model": "FABIA",
     "an": 2009,
@@ -245,182 +245,9 @@ export const masini: Masina[] = [
     "combustibil": "Benzina",
     "cutieViteze": "Manuala",
     "motor": "1.2",
-    "putere": 70,
-    "caroserie": "Sedan",
-    "disponibil": "Vandut",
-    "galerie": [
-      "/images/masini/skoda-fabia-2009 (x2)/01.jpg"
-    ]
-  },
-  {
-    "_id": "2746a5a8-20d3-4b35-8be5-a1effe407ffd",
-    "slug": "opel-astra-h-2006",
-    "marca": "OPEL ",
-    "model": "ASTRA H",
-    "an": 2006,
-    "pret": 2500,
-    "combustibil": "Benzina",
-    "cutieViteze": "Manuala",
-    "motor": "1.6",
-    "caroserie": "Sedan",
-    "disponibil": "Vandut",
-    "galerie": [
-      "/images/masini/opel-astra-h-2006/01.jpg"
-    ]
-  },
-  {
-    "_id": "eadea212-6744-4ae5-9774-b7d14415c5d5",
-    "slug": "bmw-320d-m47",
-    "marca": "BMW 320D",
-    "model": "M47",
-    "an": 2006,
-    "pret": 5999,
-    "combustibil": "Diesel",
-    "cutieViteze": "Manuala",
-    "motor": "2.0",
-    "putere": 163,
+    "putere": 60,
     "caroserie": "Hatchback",
-    "disponibil": "Vandut",
-    "galerie": [
-      "/images/masini/bmw-320d-m47/01.jpg"
-    ]
-  },
-  {
-    "_id": "f29421b7-044e-4af1-b8e5-c8862a221c04",
-    "slug": "vw-passat-2008",
-    "marca": "VW ",
-    "model": "Passat",
-    "an": 2008,
-    "pret": 4400,
-    "combustibil": "Diesel",
-    "cutieViteze": "Manuala",
-    "motor": "2.0 TDI",
-    "caroserie": "Sedan",
-    "disponibil": "Vandut",
-    "galerie": [
-      "/images/masini/vw-passat-2008/01.jpg"
-    ]
-  },
-  {
-    "_id": "fe57e922-c8d8-4657-8f1f-4b4ab98861c6",
-    "slug": "bmw-320d-mpacket-2006",
-    "marca": "BMW 320D",
-    "model": "MPACKET",
-    "an": 2006,
-    "pret": 5399,
-    "combustibil": "Diesel",
-    "cutieViteze": "Manuala",
-    "motor": "2.0 TDI",
-    "caroserie": "Hatchback",
-    "disponibil": "Vandut",
-    "galerie": [
-      "/images/masini/bmw-320d-mpacket-2006/01.jpg"
-    ]
-  },
-  {
-    "_id": "f8c06df3-4152-4231-ba63-d74ac4bed719",
-    "slug": "bmw-320d-m47-2006",
-    "marca": "BMW",
-    "model": "320D M47",
-    "an": 2006,
-    "pret": 6299,
-    "combustibil": "Diesel",
-    "cutieViteze": "Manuala",
-    "motor": "2.0",
-    "putere": 163,
-    "caroserie": "Hatchback",
-    "disponibil": "Vandut",
-    "galerie": [
-      "/images/masini/bmw-320d-m47-2006/01.jpg"
-    ]
-  },
-  {
-    "_id": "aeb998d7-52e9-411b-8472-44cc16d43fb3",
-    "slug": "vw-passat-highline-2010",
-    "marca": "VW PASSAT",
-    "model": "HIGHLINE",
-    "an": 2010,
-    "pret": 4799,
-    "combustibil": "Diesel",
-    "cutieViteze": "Manuala",
-    "motor": "2.0 TDI",
-    "putere": 140,
-    "caroserie": "Hatchback",
-    "disponibil": "Vandut",
-    "galerie": [
-      "/images/masini/vw-passat-highline-2010/01.jpg"
-    ]
-  },
-  {
-    "_id": "136e178b-a41b-4b47-98f5-4e3e759bd062",
-    "slug": "skoda-fabia-2009",
-    "marca": "Skoda ",
-    "model": "Fabia",
-    "an": 2009,
-    "pret": 2699,
-    "combustibil": "Benzina",
-    "cutieViteze": "Manuala",
-    "motor": "1.2",
-    "putere": 70,
-    "caroserie": "Sedan",
-    "disponibil": "Vandut",
-    "galerie": [
-      "/images/masini/skoda-fabia-2009/01.jpg"
-    ]
-  },
-  {
-    "_id": "2f8950c8-eb96-45d0-b004-2a3918e34622",
-    "slug": "bmw-e90-2011",
-    "marca": "BMW",
-    "model": "E90",
-    "an": 2011,
-    "pret": 5499,
-    "combustibil": "Benzina",
-    "cutieViteze": "Manuala",
-    "motor": "2.0L",
-    "putere": 163,
-    "caroserie": "Hatchback",
-    "disponibil": "Vandut",
-    "galerie": [
-      "/images/masini/bmw-e90-2011/01.jpg"
-    ]
-  },
-  {
-    "_id": "f3200ae4-fa09-441f-9615-51ed9502d1e4",
-    "slug": "vw-passat-highline",
-    "marca": "VW PASSAT",
-    "model": "HIGHLINE",
-    "an": 2008,
-    "pret": 4499,
-    "combustibil": "Diesel",
-    "cutieViteze": "Manuala",
-    "motor": "2.0 TDI",
-    "putere": 140,
-    "caroserie": "Hatchback",
-    "disponibil": "Vandut",
-    "galerie": [
-      "/images/masini/vw-passat-highline/01.jpg"
-    ]
-  },
-  {
-    "_id": "9fec058c-3a26-4fa2-b3ab-0c5c13fc4a59",
-    "slug": "bmw-320d-2006",
-    "marca": "BMW",
-    "model": "320D ",
-    "an": 2006,
-    "pret": 5799,
-    "combustibil": "Diesel",
-    "cutieViteze": "Manuala",
-    "motor": "2.0",
-    "putere": 163,
-    "caroserie": "Hatchback",
-    "culoare": "Gri",
-    "nrUsi": 4,
-    "tractiune": "FWD",
-    "inspectieTehnica": "-",
-    "disponibil": "Vandut",
-    "galerie": [
-      "/images/masini/bmw-320d-2006/01.jpg"
-    ]
+    "disponibil": "Disponibil",
+    "galerie": []
   }
 ];

68c19b708c4108f8656d13eb6276111ac1cf4a5b
backstagelevelone-crypto <backstagelevelone@gmail.com>
2026-07-12 15:57:55 +0300

    Update masini.ts

--- cosram-auto-main/src/data/masini.ts
+++ cosram-auto-main/src/data/masini.ts
@@ -149,7 +149,7 @@ export const masini: Masina[] = [
     "model": "520D",
     "an": 2006,
     "kilometraj": 309000,
-    "pret": 5499,
+    "pret": 4999,
     "combustibil": "Diesel",
     "cutieViteze": "Manuala",
     "motor": "M47",

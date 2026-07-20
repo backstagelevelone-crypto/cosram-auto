27f316526e546a5799d56b5350471e1698e23d13
backstagelevelone-crypto <backstagelevelone@gmail.com>
2026-07-21 01:21:11 +0300

    Update layout.tsx

--- cosram-auto-main/src/app/layout.tsx
+++ cosram-auto-main/src/app/layout.tsx
@@ -91,7 +91,7 @@ export default function RootLayout({
           `}
         </Script>
 
-        {/* Noul Meta Pixel Oficial Curat Cosram.ronew */}
+        {/* Meta Pixel Tradus Corect pentru Next.js (ID: 1582059960261179) */}
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -100,14 +100,23 @@ export default function RootLayout({
               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
               if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
               n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e);
+              t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://facebook.net');
+              'https://connect.facebook.net/en_US/fbevents.js');
               fbq('init', '1582059960261179');
               fbq('track', 'PageView');
             `,
           }}
         />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1582059960261179&ev=PageView&noscript=1"
+            alt="fb-pixel"
+          />
+        </noscript>
 
         {children}
         

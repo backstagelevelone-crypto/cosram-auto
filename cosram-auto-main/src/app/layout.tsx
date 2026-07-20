a04c99712ab638ae40c7f75d29714e0704dd6c17
backstagelevelone-crypto <backstagelevelone@gmail.com>
2026-07-21 00:38:34 +0300

    Update layout.tsx

--- cosram-auto-main/src/app/layout.tsx
+++ cosram-auto-main/src/app/layout.tsx
@@ -89,6 +89,23 @@ export default function RootLayout({
             gtag('config', 'AW-16466740120');
           `}
         </Script>
+
+        {/* Meta Pixel Cosram Auto */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e);
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://facebook.net');
+            fbq('init', '1620663109141493');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
         {children}
         <script
           type="application/ld+json"

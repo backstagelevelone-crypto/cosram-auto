e458b900885d241fc938fa102506978f182e5a33
backstagelevelone-crypto <backstagelevelone@gmail.com>
2026-07-22 16:37:42 +0300

    Update route.ts

--- cosram-auto-main/src/app/feed/meta.xml/route.ts
+++ cosram-auto-main/src/app/feed/meta.xml/route.ts
@@ -1,7 +1,43 @@
+import { masini } from "@/data/masini";
+
 export async function GET() {
-  return new Response("Meta feed OK", {
+  const baseUrl = "https://cosram.ro";
+
+  const masiniDisponibile = masini.filter(
+    (masina) => masina.disponibil === "Disponibil"
+  );
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<vehicles>
+${masiniDisponibile
+  .map(
+    (masina) => `
+  <vehicle>
+    <id>${masina._id}</id>
+
+    <title>
+      ${masina.marca ?? ""} ${masina.model ?? ""} ${masina.an ?? ""}
+    </title>
+
+    <price>${masina.pret ?? ""} EUR</price>
+
+    <image_link>
+      ${baseUrl}${masina.galerie?.[0] ?? ""}
+    </image_link>
+
+    <url>
+      ${baseUrl}/masini/${masina.slug}
+    </url>
+
+  </vehicle>
+`
+  )
+  .join("")}
+</vehicles>`;
+
+  return new Response(xml, {
     headers: {
-      "Content-Type": "text/plain",
+      "Content-Type": "application/xml",
     },
   });
 }

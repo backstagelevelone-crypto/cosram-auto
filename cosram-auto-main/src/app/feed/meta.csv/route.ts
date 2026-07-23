17b8cd8e8aa126f7b68a228997f201811bfbce48
backstagelevelone-crypto <backstagelevelone@gmail.com>
2026-07-23 12:56:18 +0300

    Update route.ts

--- cosram-auto-main/src/app/feed/meta.csv/route.ts
+++ cosram-auto-main/src/app/feed/meta.csv/route.ts
@@ -1,14 +1,14 @@
 export const dynamic = "force-static";
 
 export async function GET() {
-  const csv = `vehicle_id,title,description,link,image_link,price,currency,make,model,year,mileage,state_of_vehicle,body_style,fuel_type,transmission,availability,address
-0165820a-0ea1-4442-a4e8-483e6af91e17,Renault Megane 2012,Renault Megane Diesel Manuala,https://cosram.ro/masini/Renaultmegane2012,https://cosram.ro/images/masini/Renaultmegane2012/1.jpg,5499,RON,Renault,Megane,2012,268000,USED,HATCHBACK,DIESEL,MANUAL,AVAILABLE,"Strada Toamnei nr.36 Bragareasa Buzau Romania"
-0165820a-0ea1-4442-a4e8-483e6af91e21,Volkswagen Passat 2013,Volkswagen Passat Diesel Manuala,https://cosram.ro/masini/passatb72.0tdi2013,https://cosram.ro/images/masini/passatb72.0tdi2013/1.jpg,6999,RON,Volkswagen,Passat,2013,227000,USED,SEDAN,DIESEL,MANUAL,AVAILABLE,"Strada Toamnei nr.36 Bragareasa Buzau Romania"
-0165820a-0ea1-4442-a4e8-483e6af91ee7,Ford Focus 2012,Ford Focus Benzina Manuala,https://cosram.ro/masini/ford-focus-2012,https://cosram.ro/images/masini/ford-focus-2012/01.jpg,3999,RON,Ford,Focus,2012,210000,USED,HATCHBACK,GASOLINE,MANUAL,AVAILABLE,"Strada Toamnei nr.36 Bragareasa Buzau Romania"
-aeb998d7-52e9-411b-8472-44cc16d43fb3,Audi A4 2006,Audi A4 Diesel Manuala,https://cosram.ro/masini/Audi-A4-B7-Avant,https://cosram.ro/images/masini/audia4b7/a41.jpeg,4499,RON,Audi,A4,2006,180000,USED,SEDAN,DIESEL,MANUAL,AVAILABLE,"Strada Toamnei nr.36 Bragareasa Buzau Romania"
-d33fc642-c609-49b0-9714-7aa7010d0f66,Skoda Fabia 2009,Skoda Fabia Benzina Manuala,https://cosram.ro/masini/skoda-fabia-2009-portocaliu,https://cosram.ro/images/masini/skoda-fabia-2009-portocaliu/01.jpg,2999,RON,Skoda,Fabia,2009,210000,USED,HATCHBACK,GASOLINE,MANUAL,AVAILABLE,"Strada Toamnei nr.36 Bragareasa Buzau Romania"
-99a0dc79-e6ce-4fae-913c-06687b37bf65,BMW 520D 2006,BMW 520D Diesel Manuala,https://cosram.ro/masini/bmw-520d-2005,https://cosram.ro/images/masini/bmw-520d-2005/1.jpg,4999,RON,BMW,520D,2006,309000,USED,SEDAN,DIESEL,MANUAL,AVAILABLE,"Strada Toamnei nr.36 Bragareasa Buzau Romania"
-4ede9d12-f048-4919-8f46-2923f7abb083,Volkswagen Golf 5 2006,Volkswagen Golf Diesel Manuala,https://cosram.ro/masini/vw-golf-5-2006,https://cosram.ro/images/masini/vw-golf-5-2006/01.jpg,2999,RON,Volkswagen,Golf 5,2006,100000,USED,HATCHBACK,DIESEL,MANUAL,AVAILABLE,"Strada Toamnei nr.36 Bragareasa Buzau Romania"
+  const csv = `vehicle_id,title,description,url,image,price,currency,make,model,year,mileage.value,mileage.unit,state_of_vehicle,body_style,fuel_type,transmission,availability
+0165820a-0ea1-4442-a4e8-483e6af91e17,Renault Megane 2012,Renault Megane Diesel Manuala,https://cosram.ro
+0165820a-0ea1-4442-a4e8-483e6af91e21,Volkswagen Passat 2013,Volkswagen Passat Diesel Manuala,https://cosram.ro
+0165820a-0ea1-4442-a4e8-483e6af91ee7,Ford Focus 2012,Ford Focus Benzina Manuala,https://cosram.ro
+aeb998d7-52e9-411b-8472-44cc16d43fb3,Audi A4 2006,Audi A4 Diesel Manuala,https://cosram.ro
+d33fc642-c609-49b0-9714-7aa7010d0f66,Skoda Fabia 2009,Skoda Fabia Benzina Manuala,https://cosram.ro
+99a0dc79-e6ce-4fae-913c-06687b37bf65,BMW 520D 2006,BMW 520D Diesel Manuala,https://cosram.ro
+4ede9d12-f048-4919-8f46-2923f7abb083,Volkswagen Golf 5 2006,Volkswagen Golf Diesel Manuala,https://cosram.ro 5,2006,100000,KM,USED,HATCHBACK,DIESEL,MANUAL,AVAILABLE
 `;
 
   return new Response(csv, {

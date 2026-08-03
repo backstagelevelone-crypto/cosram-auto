8372a9d7e6a4b689b89891fbd45a3be5310e9f67
backstagelevelone-crypto <backstagelevelone@gmail.com>
2026-08-03 12:42:59 +0300

    Update LeadsRealtime.tsx

--- cosram-auto-main/src/components/LeadsRealtime.tsx
+++ cosram-auto-main/src/components/LeadsRealtime.tsx
@@ -1,5 +1,31 @@
 "use client";
 
+import { useEffect } from "react";
+import { supabaseBrowser } from "@/lib/supabase-browser";
+
 export default function LeadsRealtime() {
+  useEffect(() => {
+    console.log("Realtime pornit");
+
+    const channel = supabaseBrowser
+      .channel("leads-realtime")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "leads",
+        },
+        (payload) => {
+          console.log("Realtime:", payload);
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabaseBrowser.removeChannel(channel);
+    };
+  }, []);
+
   return null;
 }
